ecc532e62443ee07c2988f4d04ca1bdcb021ec8b
Vladyslava Ovsiienko <vladyslava.ovsiienko@nure.ua>
2021-11-15 16:48:08 +0200

    Adding setup, takeDown methods to BookBuyerAgent

--- lab1/src/main/java/BookBuyerAgent.java
+++ lab1/src/main/java/BookBuyerAgent.java
@@ -1,2 +1,31 @@
-public class BookBuyerAgent {
+import jade.core.Agent;
+import jade.core.AID;
+public class BookBuyerAgent extends Agent{
+    // title of the book to buy
+    private String targetBookTitle;
+    // list of known seller agents
+    private AID[] sellerAgents = {new AID("seller1", AID.ISLOCALNAME),
+            new AID("seller2", AID.ISLOCALNAME)};
+
+    // Put agent initializations
+     protected void setup() {
+    // Printout a welcome message
+         System.out.println("Hello! Buyer-agent "+getAID().getName()+" is ready.");
+    // Get title of  book to buy
+         Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+          targetBookTitle = (String) args[0];
+          System.out.println("Trying to buy "+targetBookTitle);
+         }
+          else {
+         // terminate
+            System.out.println("No book title specified");
+            doDelete();
+          }
+     }
+     // Put agent clean-up operations here
+    protected void takeDown() {
+     System.out.println("Buyer-agent "+getAID().getName()+" terminating.");
+     }
 }
+
