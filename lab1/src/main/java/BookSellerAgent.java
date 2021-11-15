bd9f73664d85095469420f4ae1fd48e7a0179b1d
Vladyslava Ovsiienko <vladyslava.ovsiienko@nure.ua>
2021-11-15 18:51:15 +0200

    Adding registration and deregistration for book-selling service

--- lab1/src/main/java/BookSellerAgent.java
+++ lab1/src/main/java/BookSellerAgent.java
@@ -1,27 +1,54 @@
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 import java.util.*;
 public class BookSellerAgent extends Agent{
-    // The catalogue of books for sale (maps the title of a book to its price)
-    private Hashtable catalogue;
-    // The GUI by means of which the user can add books in the catalogue
+     // The catalogue of books for sale (maps the title of a book to its price)
+     private Hashtable catalogue;
+     // The GUI by means of which the user can add books in the catalogue
      private BookSellerGui myGui;
 
      protected void setup() {
-         catalogue = new Hashtable();
-    // Create and show the GUI
+     catalogue = new Hashtable();
+     // Create and show the GUI
      myGui = new BookSellerGui(this);
      myGui.show();
-    // Add the behaviour serving requests for offer from buyer agents
-         addBehaviour(new OfferRequestsServer());
-         // Add the behaviour serving purchase orders from buyer agents
-         addBehaviour(new PurchaseOrdersServer());
+
+     // Register the book-selling service in the yellow pages
+     DFAgentDescription dfd = new DFAgentDescription();
+     dfd.setName(getAID());
+
+     ServiceDescription sd = new ServiceDescription();
+     sd.setType("book-selling");
+     sd.setName("JADE-book-trading");
+     dfd.addServices(sd);
+
+     try {
+             DFService.register(this, dfd);
+     } catch (FIPAException fe) {
+             fe.printStackTrace();
+     }
+     // Add the behaviour serving requests for offer from buyer agents
+     addBehaviour(new OfferRequestsServer());
+     // Add the behaviour serving purchase orders from buyer agents
+     addBehaviour(new PurchaseOrdersServer());
+
      }
     // Put agent clean-up operations here
     protected void takeDown() {
+        // Deregister from the yellow
+         try {
+            DFService.deregister(this);
+         }
+         catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
         // Close the GUI
         myGui.dispose();
         System.out.println("Seller-agent "+getAID().getName()+" terminating.");
@@ -55,10 +82,10 @@ public class BookSellerAgent extends Agent{
                 reply.setContent("not-available");
             }
             myAgent.send(reply);
-        }
-        else{
-            block();
             }
+            else{
+                block();
+                }
         }
     }
 
@@ -78,7 +105,8 @@ public class BookSellerAgent extends Agent{
                     reply.setContent("not-available");
                 }
                 myAgent.send(reply);
-            } else {
+            }
+            else {
                 block();
             }
         }
